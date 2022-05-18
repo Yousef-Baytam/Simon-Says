96ddd6f5317288c12595c7ffdc19d17d9633bb6d
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-18 22:29:27 +0300

    Defined audio files vars

--- js/app.js
+++ js/app.js
@@ -4,8 +4,13 @@ const body = document.querySelector('body')
 let steps = []
 let level = 1
 let UserInputs = []
+const green = new Audio("http://127.0.0.1:5500/assets/green.mp3")
+const red = new Audio("http://127.0.0.1:5500/assets/red.mp3")
+const yellow = new Audio("http://127.0.0.1:5500/assets/yellow.mp3")
+const blue = new Audio("http://127.0.0.1:5500/assets/blue.mp3")
+const wrong = new Audio("http://127.0.0.1:5500/assets/wrong.mp3")
 
-body.addEventListener('click', play, { once: true })
+body.addEventListener('click', start, { once: true })
 
 for (let box of boxes) {
     box.addEventListener('click', (e) => {
@@ -16,12 +21,20 @@ for (let box of boxes) {
             return
         } else if (UserInputs.length === steps.length) {
             level++
-            play()
+            e.target.id === 'one' ? green.play()
+                : e.target.id === 'two' ? blue.play()
+                    : e.target.id === 'three' ? yellow.play()
+                        : blue.play()
+            start()
         }
+        e.target.id === 'one' ? green.play()
+            : e.target.id === 'two' ? blue.play()
+                : e.target.id === 'three' ? yellow.play()
+                    : blue.play()
     })
 }
 
-function play() {
+function start() {
     title.innerText = `Level ${ level }`
     UserInputs = []
     levelStep(steps)
